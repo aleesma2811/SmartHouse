9eee494d9625a2304d51e3a46a6ffaa704fabb28
Alejandra Escallada Martínez <aleescallada@MBAlejandra-135.local>
2026-08-27 22:35:31 -0600

    Se implementa patrón de diseño (Repository Pattern)

--- back/main.go
+++ back/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/back/db"
 	"github.com/back/models"
+	"github.com/back/repository"
 	"github.com/back/routes"
 	"github.com/gorilla/mux"
 )
@@ -14,21 +15,30 @@ func main() {
 
 	db.DB.AutoMigrate(models.Plug{})
 	db.DB.AutoMigrate(models.Room{})
+
+	// Inyección de dependencias
+	roomRepo := repository.NewRoomRepository(db.DB)
+	plugRepo := repository.NewPlugRepository(db.DB)
+
+	roomHandler := routes.NewRoomHandler(roomRepo)
+	plugHandler := routes.NewPlugHandler(plugRepo)
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
+
 	// Rooms routes
-	r.HandleFunc("/rooms", routes.GetRoomsHandler).Methods("GET")
-	r.HandleFunc("/rooms/{id}", routes.GetRoomHandler).Methods("GET")
-	r.HandleFunc("/rooms", routes.PostRoomsHandler).Methods("POST")
-	r.HandleFunc("/rooms/{id}", routes.DeleteRoomsHandler).Methods("DELETE")
+	r.HandleFunc("/rooms", roomHandler.GetRoomsHandler).Methods("GET")
+	r.HandleFunc("/rooms/{id}", roomHandler.GetRoomHandler).Methods("GET")
+	r.HandleFunc("/rooms", roomHandler.PostRoomsHandler).Methods("POST")
+	r.HandleFunc("/rooms/{id}", roomHandler.DeleteRoomsHandler).Methods("DELETE")
 
 	// Plugs routes
-	r.HandleFunc("/plugs", routes.GetPlugsHandler).Methods("GET")
-	r.HandleFunc("/plugs/{id}", routes.GetPlugHandler).Methods("GET")
-	r.HandleFunc("/plugs", routes.CreatePlugHandler).Methods("POST")
-	r.HandleFunc("/plugs/{id}", routes.UpdatePlugHandler).Methods("PUT")
-	r.HandleFunc("/plugs/{id}", routes.DeletePlugsHandler).Methods("DELETE")
+	r.HandleFunc("/plugs", plugHandler.GetPlugsHandler).Methods("GET")
+	r.HandleFunc("/plugs/{id}", plugHandler.GetPlugHandler).Methods("GET")
+	r.HandleFunc("/plugs", plugHandler.PostPlugsHandler).Methods("POST")
+	r.HandleFunc("/plugs/{id}", plugHandler.UpdatePlugHandler).Methods("PUT")
+	r.HandleFunc("/plugs/{id}", plugHandler.DeletePlugsHandler).Methods("DELETE")
 
 	http.ListenAndServe(":4000", r)
 }
